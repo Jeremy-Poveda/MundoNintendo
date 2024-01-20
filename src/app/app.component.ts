568a3a3d987b300eb147da0da7ab747906417bc3
JeryRPG <jerypovedag@gmail.com>
2024-01-19 20:15:41 -0500

    Componente header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { IndexComponent } from './pages/index/index.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, IndexComponent, ProductsComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
